32d081855577edaa643c10540c6fde91271c3aae
Alexei Koshelev <Basher0303@yandex.ru>
2024-01-12 16:00:37 +0300

    Исправление проблемы с CORS на backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -22,7 +22,11 @@ mongoose
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+	origin: "http://localhost:5173",
+	credentials: true,
+	methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+}));
 app.use("/api", routes);
 app.use(errorMiddleware);
 
